fb8b334e9838bb4d6f179ab68d49aa4ddf44f998
take-iwiw <take.iwiw2222@gmail.com>
2020-05-09 11:44:52 +0900

    implemented project usigng pipline model for classification

--- project_pipeline/main.cpp
+++ project_pipeline/main.cpp
@@ -16,22 +16,18 @@
 #include "tensorflow/lite/kernels/register.h"
 
 /* for Edge TPU */
-#ifdef USE_EDGETPU
 #include "edgetpu.h"
 #include "edgetpu_c.h"
 #include "src/cpp/pipeline/pipelined_model_runner.h"
 #include "src/cpp/pipeline/utils.h"
-#endif
 
 /*** Macro ***/
 /* Model parameters */
-#ifdef USE_EDGETPU
-#define USE_EDGETPU_DELEGATE
-#define MODEL_FILENAME RESOURCE"/deeplabv3_mnv2_dm05_pascal_quant_edgetpu.tflite"
-#else
-#define MODEL_FILENAME RESOURCE"/deeplabv3_mnv2_dm05_pascal_quant.tflite"
-#endif
-#define LABEL_NAME     RESOURCE"/pascal_voc_segmentation_labels.txt"
+#define MODEL_SEGMENTS_NUM 3
+#define MODEL_FILENAME_BASE RESOURCE"/mobilenet_v2_1.0_224_quant"
+#define LABEL_NAME          RESOURCE"/imagenet_labels.txt"
+#define MODEL_WIDTH 224
+#define MODEL_HEIGHT 224
 
 /* Settings */
 #define LOOP_NUM_FOR_TIME_MEASUREMENT 100
@@ -50,75 +46,8 @@ enum class EdgeTpuType {
 };
 
 /*** Function ***/
-static void displayModelInfo(const tflite::Interpreter* interpreter)
-{
-	const auto& inputIndices = interpreter->inputs();
-	int inputNum = (int)inputIndices.size();
-	printf("Input num = %d\n", inputNum);
-	for (int i = 0; i < inputNum; i++) {
-		auto* tensor = interpreter->tensor(inputIndices[i]);
-		for (int j = 0; j < tensor->dims->size; j++) {
-			printf("    tensor[%d]->dims->size[%d]: %d\n", i, j, tensor->dims->data[j]);
-		}
-		if (tensor->type == kTfLiteUInt8) {
-			printf("    tensor[%d]->type: quantized\n", i);
-			printf("    tensor[%d]->params.outputZeroPoint, scale: %d, %f\n", i, tensor->params.zero_point, tensor->params.scale);
-		} else {
-			printf("    tensor[%d]->type: not quantized\n", i);
-		}
-	}
-
-	const auto& outputIndices = interpreter->outputs();
-	int outputNum = (int)outputIndices.size();
-	printf("Output num = %d\n", outputNum);
-	for (int i = 0; i < outputNum; i++) {
-		auto* tensor = interpreter->tensor(outputIndices[i]);
-		for (int j = 0; j < tensor->dims->size; j++) {
-			printf("    tensor[%d]->dims->size[%d]: %d\n", i, j, tensor->dims->data[j]);
-		}
-		if (tensor->type == kTfLiteUInt8) {
-			printf("    tensor[%d]->type: quantized\n", i);
-			printf("    tensor[%d]->params.outputZeroPoint, scale: %d, %f\n", i, tensor->params.zero_point, tensor->params.scale);
-		} else {
-			printf("    tensor[%d]->type: not quantized\n", i);
-		}
-	}
-}
-
-static void extractTensorAsFloatVector(const TfLiteTensor* tensor, std::vector<float> &output)
-{
-	int dataNum = 1;
-	for (int i = 0; i < tensor->dims->size; i++) {
-		dataNum *= tensor->dims->data[i];
-	}
-	output.resize(dataNum);
-	if (tensor->type == kTfLiteUInt8) {
-		const auto *valUint8 = tensor->data.uint8;
-		for (int i = 0; i < dataNum; i++) {
-			float valFloat = (valUint8[i] - tensor->params.zero_point) * tensor->params.scale;
-			output[i] = valFloat;
-		}
-	} else {
-		for (int i = 0; i < dataNum; i++) {
-			float valFloat = tensor->data.f[i];
-			output[i] = valFloat;
-		}
-	}
-}
-
-static void readLabel(const char* filename, std::vector<std::string> & labels)
-{
-	std::ifstream ifs(filename);
-	if (ifs.fail()) {
-		printf("failed to read %s\n", filename);
-		return;
-	}
-	std::string str;
-	while (getline(ifs, str)) {
-		labels.push_back(str);
-	}
-}
-std::vector<std::shared_ptr<edgetpu::EdgeTpuContext>> PrepareEdgeTpuContexts(
+/* the following function is from https://github.com/google-coral/edgetpu/blob/master/src/cpp/examples/model_pipelining.cc */
+static std::vector<std::shared_ptr<edgetpu::EdgeTpuContext>> PrepareEdgeTpuContexts(
 	int num_tpus, EdgeTpuType device_type) {
 	auto get_available_tpus = [](EdgeTpuType device_type) {
 		const auto& all_tpus =
@@ -170,7 +99,9 @@ std::vector<std::shared_ptr<edgetpu::EdgeTpuContext>> PrepareEdgeTpuContexts(
 
 	return edgetpu_contexts;
 }
-std::unique_ptr<tflite::Interpreter> BuildEdgeTpuInterpreter(
+
+/* the following function is from https://github.com/google-coral/edgetpu/blob/master/src/cpp/examples/model_pipelining.cc */
+static std::unique_ptr<tflite::Interpreter> BuildEdgeTpuInterpreter(
 	const tflite::FlatBufferModel& model, edgetpu::EdgeTpuContext* context) {
 	tflite::ops::builtin::BuiltinOpResolver resolver;
 	resolver.AddCustom(edgetpu::kCustomOp, edgetpu::RegisterCustomOp());
@@ -192,116 +123,152 @@ std::unique_ptr<tflite::Interpreter> BuildEdgeTpuInterpreter(
 	return interpreter;
 }
 
+static std::vector<std::string> getSegmentedModelPathList(std::string modelPathBase, int numSegment)
+{
+	std::vector<std::string> pathList;
+	for (int i = 0; i < numSegment; i++) {
+		std::string path;
+		path = modelPathBase + "_segment_" + std::to_string(i) + "_of_" + std::to_string(numSegment) + "_edgetpu.tflite";
+		pathList.push_back(path);
+	}
+	return pathList;
+}
+
+static coral::PipelineTensor createPipelineTensor(const tflite::Interpreter* interpreter, coral::Allocator* allocator, int index, uint8_t* data, int size)
+{
+	const auto* inputTensor = interpreter->tensor(index);
+	TFLITE_MINIMAL_CHECK(inputTensor->bytes == size);
+	TFLITE_MINIMAL_CHECK(inputTensor->type == kTfLiteUInt8);
+
+	coral::PipelineTensor pipelineTensor;
+	pipelineTensor.data.data = allocator->alloc(inputTensor->bytes);		// need to be released after consumed
+	pipelineTensor.bytes = inputTensor->bytes;
+	pipelineTensor.type = inputTensor->type;
+	memcpy(pipelineTensor.data.data, data, size);
+
+	return pipelineTensor;
+}
+
+
+static void extractTensorAsFloatVector(const tflite::Interpreter *interpreter, const int index, coral::PipelineTensor &pipelineTensor, std::vector<float> &output)
+{
+	const TfLiteTensor* tensor = interpreter->tensor(index);
+	int dataNum = 1;
+	for (int i = 0; i < tensor->dims->size; i++) {
+		dataNum *= tensor->dims->data[i];
+	}
+	output.resize(dataNum);
+	if (tensor->type == kTfLiteUInt8) {
+		//const auto *valUint8 = interpreter->typed_tensor<uint8_t>(index);
+		const auto *valUint8 = pipelineTensor.data.uint8;
+		for (int i = 0; i < dataNum; i++) {
+			float valFloat = (valUint8[i] - tensor->params.zero_point) * tensor->params.scale;
+			output[i] = valFloat;
+		}
+	} else {
+		//const auto *valFloat = interpreter->typed_tensor<float>(index);
+		const auto *valFloat = pipelineTensor.data.f;
+		for (int i = 0; i < dataNum; i++) {
+			output[i] = valFloat[i];
+		}
+	}
+}
+
+static void readLabel(const char* filename, std::vector<std::string> & labels)
+{
+	std::ifstream ifs(filename);
+	if (ifs.fail()) {
+		printf("failed to read %s\n", filename);
+		return;
+	}
+	std::string str;
+	while (getline(ifs, str)) {
+		labels.push_back(str);
+	}
+}
+
+
 int main()
 {
 	/*** Initialize ***/
-	/* read label */
-	std::vector<std::string> labels;
-	readLabel(LABEL_NAME, labels);
+	/* Prepare Edge TPU contexts */
+	auto contexts = PrepareEdgeTpuContexts(MODEL_SEGMENTS_NUM, EdgeTpuType::kAny);
+	TFLITE_MINIMAL_CHECK(!contexts.empty());
 
-	/* Create interpreters */
-	int num_segments = 2;
-	auto contexts = PrepareEdgeTpuContexts(num_segments, EdgeTpuType::kAny);
-
-	std::vector<std::unique_ptr<tflite::Interpreter>> managed_interpreters(num_segments);
-	std::vector<tflite::Interpreter*> interpreters(num_segments);
-	std::vector<std::unique_ptr<tflite::FlatBufferModel>> models(num_segments);
-	models[0] = tflite::FlatBufferModel::BuildFromFile("resource/inception_v3_299_quant_segment_0_of_2_edgetpu.tflite");
-	models[1] = tflite::FlatBufferModel::BuildFromFile("resource/inception_v3_299_quant_segment_1_of_2_edgetpu.tflite");
-
-	for (int i = 0; i < num_segments; ++i) {
-		managed_interpreters[i] =
-			BuildEdgeTpuInterpreter(*(models[i]), contexts[i].get());
-		if (managed_interpreters[i] == nullptr) {
-			return 1;
-		}
-		interpreters[i] = managed_interpreters[i].get();
+	/* Build model pipeline runner */
+	std::vector<std::string> modelPathList = getSegmentedModelPathList(MODEL_FILENAME_BASE, MODEL_SEGMENTS_NUM);
+	std::vector<std::unique_ptr<tflite::Interpreter>> managedInterpreters(MODEL_SEGMENTS_NUM);
+	std::vector<tflite::Interpreter*> interpreters(MODEL_SEGMENTS_NUM);
+	std::vector<std::unique_ptr<tflite::FlatBufferModel>> models(MODEL_SEGMENTS_NUM);
+	for (int i = 0; i < MODEL_SEGMENTS_NUM; i++) {
+		models[i] = tflite::FlatBufferModel::BuildFromFile(modelPathList[i].c_str());
+		TFLITE_MINIMAL_CHECK(models[i] != nullptr);
+		managedInterpreters[i] = BuildEdgeTpuInterpreter(*(models[i]), contexts[i].get());
+		TFLITE_MINIMAL_CHECK(managedInterpreters[i] != nullptr);
+		interpreters[i] = managedInterpreters[i].get();
 	}
+	std::unique_ptr<coral::PipelinedModelRunner> runner(new coral::PipelinedModelRunner(interpreters));
 
-	std::unique_ptr<coral::PipelinedModelRunner> runner(
-		new coral::PipelinedModelRunner(interpreters));
-	volatile auto* input_tensor_allocator = runner->GetInputTensorAllocator();
-	volatile auto* output_tensor_allocator = runner->GetOutputTensorAllocator();
 
-//	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(MODEL_FILENAME);
-//	TFLITE_MINIMAL_CHECK(model != nullptr);
-//	tflite::ops::builtin::BuiltinOpResolver resolver;
-//	tflite::InterpreterBuilder builder(*model, resolver);
-//	std::unique_ptr<tflite::Interpreter> interpreter;
-//	builder(&interpreter);
-//	TFLITE_MINIMAL_CHECK(interpreter != nullptr);
-//	interpreter->SetNumThreads(4);
-//#ifdef USE_EDGETPU_DELEGATE
-//	size_t num_devices;
-//	std::unique_ptr<edgetpu_device, decltype(&edgetpu_free_devices)> devices(edgetpu_list_devices(&num_devices), &edgetpu_free_devices);
-//	TFLITE_MINIMAL_CHECK(num_devices > 0);
-//	const auto& device = devices.get()[0];
-//	auto* delegate = edgetpu_create_delegate(device.type, device.path, nullptr, 0);
-//	interpreter->ModifyGraphWithDelegate({delegate, edgetpu_free_delegate});
-//#endif
-//	TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
-//
-//
-//	/* Get model information */
-//	displayModelInfo(interpreter.get());
-//	const TfLiteTensor* inputTensor = interpreter->input_tensor(0);
-//	const int modelInputHeight = inputTensor->dims->data[1];
-//	const int modelInputWidth = inputTensor->dims->data[2];
-//	const int modelInputChannel = inputTensor->dims->data[3];
-//
-//
-//	/*** Process for each frame ***/
-//	/* Read input image data */
-//	cv::Mat originalImage = cv::imread(RESOURCE"/cat_dog.jpg");
-//	cv::Mat inputImage;
-//
-//	/* Pre-process and Set data to input tensor */
-//	cv::cvtColor(originalImage, inputImage, cv::COLOR_BGR2RGB);
-//	cv::resize(inputImage, inputImage, cv::Size(modelInputWidth, modelInputHeight));
-//	if (inputTensor->type == kTfLiteUInt8) {
-//		inputImage.convertTo(inputImage, CV_8UC3);
-//		memcpy(inputTensor->data.int8, inputImage.reshape(0, 1).data, sizeof(int8_t) * 1 * modelInputWidth * modelInputHeight * modelInputChannel);
-//	} else {
-//		inputImage.convertTo(inputImage, CV_32FC3, 1.0 / 255);
-//		memcpy(inputTensor->data.f, inputImage.reshape(0, 1).data, sizeof(float) * 1 * modelInputWidth * modelInputHeight * modelInputChannel);
-//	}
-//
-//	/* Run inference */
-//	TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-//
-//	/* Retrieve the result */
-//	int ouputWidth = interpreter->output_tensor(0)->dims->data[2];
-//	int ouputHeight = interpreter->output_tensor(0)->dims->data[1];
-//	const int64_t *outputMap;
-//	if (interpreter->output_tensor(0)->type == kTfLiteInt64) {
-//		outputMap = interpreter->output_tensor(0)->data.i64;
-//	} else {
-//		// todo
-//	}
-//	
-//	/* Display result */
-//	cv::Mat outputImage = cv::Mat::zeros(ouputHeight, ouputWidth, CV_8UC3);
-//	for (int y = 0; y < ouputHeight; y++) {
-//		for (int x = 0; x < ouputWidth; x++) {
-//			if (outputMap[y * ouputWidth + x] != 0) {
-//				const int CHANNE_NUM = 3;
-//				outputImage.data[(y * ouputWidth + x) * CHANNE_NUM] = 0xFF;
-//			}
-//		}
-//	}
-//	cv::imshow("originalImage", originalImage); cv::waitKey(1);
-//	cv::imshow("outputImage", outputImage); cv::waitKey(1);
-//
-//
-//	/*** (Optional) Measure inference time ***/
-//	const auto& t0 = std::chrono::steady_clock::now();
-//	for (int i = 0; i < LOOP_NUM_FOR_TIME_MEASUREMENT; i++) {
-//		interpreter->Invoke();
-//	}
-//	const auto& t1 = std::chrono::steady_clock::now();
-//	std::chrono::duration<double> timeSpan = t1 - t0;
-//	printf("Inference time = %f [msec]\n", timeSpan.count() * 1000.0 / LOOP_NUM_FOR_TIME_MEASUREMENT);
-//	
-//	cv::waitKey(-1);
+	/*** Process for each frame ***/
+	/* Define logics */
+	auto requestProducer = [&interpreters, &runner]() {
+		const tflite::Interpreter* interpreterFirst = interpreters[0];
+		for (int i = 0; i < LOOP_NUM_FOR_TIME_MEASUREMENT; ++i) {
+			/* Read input image data */
+			cv::Mat originalImage = cv::imread(RESOURCE"/parrot.jpg");
+			cv::imshow("test", originalImage); cv::waitKey(1);
+			/* Pre-process */
+			cv::Mat inputImage;
+			cv::cvtColor(originalImage, inputImage, cv::COLOR_BGR2RGB);
+			cv::resize(inputImage, inputImage, cv::Size(MODEL_WIDTH, MODEL_HEIGHT));
+			inputImage.convertTo(inputImage, CV_8UC3);
+
+			/* Set input data to pipeline tensor */
+			std::vector<coral::PipelineTensor> request;		// use vector but the size is one because the model has only one input
+			request.push_back(createPipelineTensor(interpreterFirst, runner->GetInputTensorAllocator(), interpreterFirst->inputs()[0], inputImage.data, sizeof(uint8_t) * 1 * inputImage.rows  * inputImage.cols * inputImage.channels()));
+
+			/* Push input tensors to be processed by the pipeline. */
+			runner->Push(request);
+		}
+
+		/* Send signals shutting down the pipeline */
+		runner->Push({});
+	};
+
+	auto requestConsumer = [&interpreters, &runner]() {
+		const tflite::Interpreter* interpreterLast = interpreters[MODEL_SEGMENTS_NUM - 1];
+		/* read label */
+		std::vector<std::string> labels;
+		readLabel(LABEL_NAME, labels);
+
+		std::vector<coral::PipelineTensor> response;
+		while (runner->Pop(&response)) {
+			/* Retrieve the result */
+			std::vector<float> outputScoreList;
+			extractTensorAsFloatVector(interpreterLast, interpreterLast->outputs()[0], response[0], outputScoreList);
+			int maxIndex = (int)(std::max_element(outputScoreList.begin(), outputScoreList.end()) - outputScoreList.begin());
+			auto maxScore = *std::max_element(outputScoreList.begin(), outputScoreList.end());
+			printf("%s (%.3f)\n", labels[maxIndex].c_str(), maxScore);
+
+			/* Release memory*/
+			coral::FreeTensors(response, runner->GetOutputTensorAllocator());
+			response.clear();
+		}
+	};
+
+	/* Start process for each frame */
+	const auto& t0 = std::chrono::steady_clock::now();
+	auto producer = std::thread(requestProducer);
+	auto consumer = std::thread(requestConsumer);
+	producer.join();
+	consumer.join();
+	const auto& t1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeSpan = t1 - t0;
+	printf("Total time = %f [msec]\n", timeSpan.count() * 1000.0 / LOOP_NUM_FOR_TIME_MEASUREMENT);
+
+	cv::waitKey(-1);
+	cv::destroyAllWindows();
+
 	return 0;
 }
